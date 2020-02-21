0d1e2c5c3388dd3ebe021b3cbae1648a461ed76d
Eduardo Henrique Rotundaro <eduardo.rotundaro@olhonocarro.com.br>
2020-02-21 18:22:17 -0300

    :lipstick: UI development

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import logo from '../../images/logo.png';
+
 export default () => {
     return (
-        <header></header>
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
+                <a className="navbar-brand" href="/">
+                    <img className="navbar--logo" src={logo} alt=""/>
+                </a>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Features</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
 }
