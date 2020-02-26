93e6ca1e6589e8de4664d63d4db484854c0b412e
Eduardo Henrique Rotundaro <eduardo.rotundaro@olhonocarro.com.br>
2020-02-26 11:17:55 -0300

    :sparkles: adding generators

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,2 +1,63 @@
-export const generateQRCode = function(){
+import QRCode from 'qrcode-generator';
+import domtoimage from 'dom-to-image';
+import {saveAs} from 'file-saver';
+
+export const generateQRCode = function(typeNumber=4, errorCorrectionLevel='L', data){
+    try{
+        let qrcd =  QRCode(typeNumber, errorCorrectionLevel);
+        qrcd.addData(data);
+        qrcd.make();
+        return qrcd.createImgTag();
+    }
+    catch(err){
+        return null;
+    }
+}
+
+export const generatePNGImage = async function(node){
+    try {
+        const dataUrl = await domtoimage.toPng(node);
+        let img = new Image();
+        img.src = dataUrl;
+        return img;
+    } catch (error) {
+        return null;
+    }
+}
+
+export const generateJPEGImage = async function(node){
+    try {
+        const dataUrl = await domtoimage.toJpeg(node);
+        let img = new Image();
+        img.src = dataUrl;
+        return img;
+    } catch (error) {
+        return null;
+    }
+}
+
+export const downloadAsPNG = async function(node){
+    try {
+        const isFileSaverSupported  = !!new Blob();
+        if(!isFileSaverSupported) return false;
+
+        const blob = await domtoimage.toBlob(node);
+        saveAs(blob, 'qrcode.png');
+        return true;
+    } catch (error) {
+        return null;
+    }
+}
+
+export const downloadAsJPEG = async function(node){
+    try {
+        const dataUrl = await domtoimage.toJpeg(node, { quality: 0.95 });
+        let link = document.createElement('a');
+        link.download = 'qrcode.jpeg';
+        link.href = dataUrl;
+        link.click();
+        return true;
+    } catch (error) {
+        return null;
+    }
 }
