84f61898588c09256d02bf0aed003a627f01d69e
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-03-02 23:23:41 -0300

    Updates

---
+++ precache-manifest.ab371ddbb027a7b0b86c1ec1e822f789.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200b58dbefc69069bed48de7ec1690b4",
+    "url": "/qrcode-badge-generator/index.html"
+  },
+  {
+    "revision": "1b5c6a86d941810a5967",
+    "url": "/qrcode-badge-generator/static/css/main.355ce468.chunk.css"
+  },
+  {
+    "revision": "4e326050be6d0b6d4c4c",
+    "url": "/qrcode-badge-generator/static/js/2.f0d5ef68.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/qrcode-badge-generator/static/js/2.f0d5ef68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5c6a86d941810a5967",
+    "url": "/qrcode-badge-generator/static/js/main.0694f9b8.chunk.js"
+  },
+  {
+    "revision": "fb83b8e92aebe13085b8",
+    "url": "/qrcode-badge-generator/static/js/runtime-main.8812974c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/qrcode-badge-generator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/qrcode-badge-generator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/qrcode-badge-generator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/qrcode-badge-generator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/qrcode-badge-generator/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
